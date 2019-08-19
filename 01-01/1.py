1a7a9305a06f25b890ee1b21d652168091e445f9
yosaphatt <yosaphat.0720@students.amikom.ac.id>
2019-08-19 13:30:06 +0700

    latihan

---
+++ 01-01/1.py
@@ -0,0 +1,25 @@
+
+
+hobby = 'catur'
+nama = 'rio'
+print('nama {0} dan memliki hobby {1}'.format(nama,hobby))
+
+print ('cobahh') 
+#komen
+
+
+print('nama {nama} dan memliki\
+ hobby {hobby}'.format(nama=nama,hobby=hobby))
+
+
+print('{0:.3f}'.format(1.0/3))
+
+
+""" 
+nama = 'yosa'
+umur = 20
+print ('nama saya saya adalah',nama)
+"""
+
+# tanda "\" digunakan agar tidak terlalu panjang di
+#code editor
---
+++ 01-01/2.py
@@ -0,0 +1,19 @@
+print('{0:.3f}'.format(1.0/3))
+
+b = "Hellostrip, World!"
+print(b[2:4])
+ 
+r=4
+print(r)
+r+=1
+print(r)
+
+# ** pangkat
+
+
+#update
+thisset = {"apple", "banana", "cherry"}
+
+thisset.update(["orange", "mango", "grapes"])
+
+print(thisset)
